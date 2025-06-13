9ef9036b83c5166dd3b7dee4090d5a5c112476ee
Rai5559 <icexbee1@gmail.com>
2025-06-12 20:53:08 -0600

    first commit

---
+++ src/test/java/com/rai69/literalura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rai69.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
